1c3e8dd1ea04a98239f5eeca52177071a6587e57
daesungra <daesungra@gmail.com>
2020-11-05 00:46:22 +0900

    core-app: Add TimeStampedModel

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,3 +1,20 @@
-from django.db import models
+from django.db.models import Model, DateTimeField
 
-# Create your models here.
+
+class TimeStampedModel(Model):
+
+    """
+    Custom TimeStamped Model
+        disc: This is abstract model.
+        It'll be implemented by applications in this project.
+    """
+
+    created = DateTimeField(auto_now_add=True)
+    updated = DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+        ordering = (
+            '-created',
+            '-updated',
+        )
