628e4e474a0224f23ed06b8efabe953ead102eae
daesungra <daesungra@gmail.com>
2020-12-13 19:36:25 +0900

    app-users: Add default user model and admin

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,13 @@
-from django.contrib import admin
+from django.contrib.admin import register as admin_register
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import User as UserModel
+
+
+@admin_register(UserModel)
+class CustomUserAdmin(UserAdmin):
+    """Default User Admin"""
+    fieldsets = UserAdmin.fieldsets
+    list_display = UserAdmin.list_display
+    ordering = ('-is_superuser', '-is_staff',)
+    list_filter = UserAdmin.list_filter
