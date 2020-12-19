782e9681fe1497d03bebac1f31110183c777874a
daesungra <daesungra@gmail.com>
2020-12-19 21:24:28 +0900

    ui: Add home url and view

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import View
+
+
+def home_view(request):
+    context = []
+    return render(request, 'home.html', context)
