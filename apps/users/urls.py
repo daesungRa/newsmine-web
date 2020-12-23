7cfb4b2ee929d16ca53ab22eff6db3d0566c81f5
daesungra <daesungra@gmail.com>
2020-12-23 18:34:44 +0900

    users: Add users login urls and views

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -3,4 +3,9 @@ from django.urls import path
 from apps.users import views as user_views
 
 
-# TODO: Make users url
+app_name = 'users'
+urlpatterns = [
+    path('login', user_views.LoginView.as_view(), name='login'),
+    # path('logout', user_views, name='logout'),
+    # path('signup', user_views, name='signup'),
+]
