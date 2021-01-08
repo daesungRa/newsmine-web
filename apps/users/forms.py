a7e1a58dc0cd8815f7bdbf64b5ec3526776ab1c8
daesungra <daesungra@gmail.com>
2021-01-08 18:37:50 +0900

    users-forms: Add Sign up form

--- apps/users/forms.py
+++ apps/users/forms.py
@@ -1,4 +1,4 @@
-from django.forms import Form, ValidationError
+from django.forms import Form, ModelForm, ValidationError
 from django.forms import CharField, EmailField, PasswordInput
 
 from apps.users.models import User as UserModel
@@ -20,3 +20,38 @@ class LoginForm(Form):
                 self.add_error('password', ValidationError('Password is wrong'))
         except UserModel.DoesNotExist as ne:
             self.add_error('username', ValidationError('User does not exist'))
+
+
+class SignUpForm(ModelForm):
+    """Signup Form detail"""
+    password = CharField(widget=PasswordInput)
+    password_confirm = CharField(widget=PasswordInput, label='Confirm Password')
+
+    class Meta:
+        model = UserModel
+        fields = ['username', 'first_name', 'last_name', 'email']
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        try:
+            UserModel.objects.get(username=username)
+            raise ValidationError('User already exists with that username/email')
+        except UserModel.DoesNotExist:
+            return username
+
+    def clean_password_confirm(self):
+        password = self.cleaned_data.get('password')
+        password_confirm = self.cleaned_data.get('password_confirm')
+        if password != password_confirm:
+            raise ValidationError('Password confirmation does not match')
+        else:
+            return password
+
+    def save(self, *args, **kwargs):
+        username = self.cleaned_data.get('username')
+        password = self.cleaned_data.get('password')
+
+        user = super().save(commit=False)
+        user.username = username
+        user.set_password(password)
+        user.save()
