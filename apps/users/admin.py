7efedeb59e9c6f9fcdd605d22c289079460388a0
daesungra <daesungra@gmail.com>
2021-01-23 01:36:22 +0900

    users-admin: Add UserModel's admin fields

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -7,7 +7,32 @@ from .models import User as UserModel
 @admin_register(UserModel)
 class CustomUserAdmin(UserAdmin):
     """Default User Admin"""
-    fieldsets = UserAdmin.fieldsets
-    list_display = UserAdmin.list_display
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            'Additional info',
+            {
+                'fields': (
+                    'nickname',
+                    'bio',
+                    'profile_image',
+                    'thumbnail_image',
+                    'language',
+                    'superuser',
+                    'login_method',
+                ),
+            }
+        ),
+    )
+    list_display = UserAdmin.list_display + (
+        'language',
+        'superuser',
+        'is_active',
+        'is_superuser',
+        'email_verified',
+        'email_secret',
+        'login_method',
+    )
     ordering = ('-is_superuser', '-is_staff',)
-    list_filter = UserAdmin.list_filter
+    list_filter = UserAdmin.list_filter + (
+        'superuser',
+    )
