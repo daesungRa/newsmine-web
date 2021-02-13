8ecaf136b93b68f6eddcb4ba62efff6f4a49b813
daesungra <daesungra@gmail.com>
2021-02-13 23:11:56 +0900

    subscriptions: Add subscription form

--- apps/subscriptions/forms.py
+++ apps/subscriptions/forms.py
@@ -1,3 +1,24 @@
-from django.forms import Form
+from django.forms import ModelForm, ValidationError
+from django.forms import CharField
 
-# TODO: Create subscription forms
+from apps.subscriptions.models import Subscription as SubscriptionModel
+
+
+class SubscriptionForm(ModelForm):
+    """Default Subscription Form"""
+    class Meta:
+        model = SubscriptionModel
+        fields = [
+            'name', 'keywords', 'exclusions', 'labels', 'description',
+            'display_count', 'cron', 'active', 'send_count',
+        ]
+
+    def clean_name(self):
+        name = self.cleaned_data.get('name', '')
+        if not name:
+            raise ValidationError(f'Name must not be blank.')
+        try:
+            SubscriptionModel.objects.get(name=name)
+            raise ValidationError(f'Name {name} already exists. Please enter another name')
+        except SubscriptionModel.DoesNotExist:
+            return name
