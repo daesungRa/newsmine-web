0207aedddc92e866494f70dce59688a2789fa990
daesungra <daesungra@gmail.com>
2021-02-17 23:40:18 +0900

    subscriptions-view: Change class view of detail url

--- apps/subscriptions/urls.py
+++ apps/subscriptions/urls.py
@@ -6,5 +6,5 @@ from apps.subscriptions import views as subscription_views
 app_name = 'subscriptions'
 urlpatterns = [
     path('', subscription_views.subscription_list, name='list'),
-    path('<int:pk>', subscription_views.SubscriptionDetail.as_view(), name='detail'),
+    path('<int:pk>', subscription_views.SubscriptionView.as_view(), name='detail'),
 ]
